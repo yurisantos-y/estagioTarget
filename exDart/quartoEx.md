6278d790a49e61fd99a6998010c4ab052afeef2e
yurisantos-y <yuri01.sp@gmail.com>
2024-11-10 00:52:47 -0300

    Estilizando as respostas

---
+++ exDart/quartoEx.md
@@ -0,0 +1,19 @@
+# Descubra a lógica:
+
+### a) 1, 3, 5, 7 -> 9
+A lógica é adicionar 2 a cada número ímpar.
+
+### b) 2, 4, 8, 16, 32, 64 -> 128
+A lógica é multiplicar cada número por 2 e manter a sequência.
+
+### c) 0, 1, 4, 9, 16, 25, 36 -> 49
+A lógica é elevar os números (1, 2, 3, 4, 5, 6 e 7) ao quadrado.
+
+### d) 4, 16, 36, 64 -> 100
+A lógica é elevar os números pares (2, 4, 6, 8 e 10) ao quadrado.
+
+### e) 1, 1, 2, 3, 5, 8 -> 13
+A lógica é a sequência de Fibonacci, onde cada número é a soma dos dois anteriores.
+
+### f) 2, 10, 12, 16, 17, 18, 19 -> 200
+A lógica aplicada é os números que começam com a letra D.
--- exDart/segundoEx.dart
+++ exDart/segundoEx.dart
@@ -1,21 +1,16 @@
 void main() {
-  // Define the string to be checked
   String inputString = "Aqui está um exemplo de string com várias letras A e a.";
 
-  // Convert the string to lowercase to make the search case-insensitive
   String lowerCaseString = inputString.toLowerCase();
 
-  // Initialize a counter for the occurrences of 'a'
   int count = 0;
 
-  // Iterate through each character in the string
   for (int i = 0; i < lowerCaseString.length; i++) {
     if (lowerCaseString[i] == 'a') {
       count++;
     }
   }
 
-  // Check if 'a' exists in the string and print the results
   if (count > 0) {
     print("A letra 'a' aparece $count vezes na string.");
   } else {
